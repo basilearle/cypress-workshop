ee894ba4fede4b604e56e0b5009414215299538a
Basil Earle <basilearle@gmail.com>
2025-05-11 16:31:40 -0300

    Initial commit

---
+++ apps/client-ui-e2e/cypress.config.ts
@@ -0,0 +1,18 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      bundler: 'vite',
+      webServerCommands: {
+        default: 'nx run client-ui:serve',
+        production: 'nx run client-ui:preview',
+      },
+      ciWebServerCommand: 'nx run client-ui:serve-static',
+    }),
+    baseUrl: 'http://localhost:4200',
+  },
+});
